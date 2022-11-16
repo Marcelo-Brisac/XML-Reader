26d120b6ea4e3bf4027db7c871c2846162177f47
Marcelo-Brisac <mbrisac@etrnty.com.br>
2022-11-16 10:04:09 -0300

    Initial commit

---
+++ readxml.py
@@ -0,0 +1,53 @@
+import pandas as pd
+f1= "09622314000109.xml"  #Macauba
+f2= "35699303000129.xml"  #Ebano
+
+def get_frame(xp):
+    try:
+        x = pd.read_xml(f1, xpath=xp)
+        x["fundo"]=["Macauba" for _ in range(len(x.index))]
+    except:
+        x = None
+    
+    try:
+        y = pd.read_xml(f2, xpath=xp)
+        y["fundo"]=["Ebano" for _ in range(len(y.index))]
+    except:
+         y=None
+
+    if y is None:
+        return x 
+    elif x is None:
+        return y
+    else:
+        return pd.concat([x,y],ignore_index=True, axis=0)
+
+tit_publico = get_frame("//arquivoposicao_4_01/fundo/titpublico")
+debentures = get_frame("//arquivoposicao_4_01/fundo/debenture")
+acoes = get_frame("//arquivoposicao_4_01/fundo/acoes")
+caixa = get_frame("//arquivoposicao_4_01/fundo/caixa")
+cotas = get_frame("//arquivoposicao_4_01/fundo/cotas")
+
+p1 = tit_publico[["isin","valorfindisp","fundo"]].copy()
+p2 = debentures[["isin","valorfindisp","fundo"]].copy()
+p3 = acoes[["codativo","valorfindisp","fundo"]].copy()
+
+caixa["valorfindisp"]=caixa["saldo"]
+p4 = caixa[["isininstituicao","valorfindisp","fundo"]]
+
+cotas["valorfindisp"]=cotas["puposicao"]*cotas["qtdisponivel"]
+p5=cotas[["cnpjfundo","valorfindisp","fundo"]].copy()
+
+p1.columns=["ID","Valor","Fundo"]
+p2.columns=["ID","Valor","Fundo"]
+p3.columns=["ID","Valor","Fundo"]
+p4.columns=["ID","Valor","Fundo"]
+p5.columns=["ID","Valor","Fundo"]
+final = pd.concat([p1,p2,p3,p4,p5],ignore_index=True, axis=0)
+
+with pd.ExcelWriter("output.xlsx") as writer:
+    final.to_excel(writer,sheet_name="resumo")
+    tit_publico.to_excel(writer,sheet_name="tit_publico")
+    debentures.to_excel(writer,sheet_name="debentures")
+    acoes.to_excel(writer,sheet_name="acoes")
+    cotas.to_excel(writer,sheet_name="cotas")
